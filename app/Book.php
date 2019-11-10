88b28b72f956ff4b1afb6fe1bd853b8680286c4a
Kai Koyamatsu <c011514454@edu.teu.ac.jp>
2019-11-10 20:27:31 +0900

    add book list

--- app/Book.php
+++ app/Book.php
@@ -4,6 +4,24 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Book
+ *
+ * @property int $id
+ * @property string $name
+ * @property int $price
+ * @property string|null $author
+ * @property \Illuminate\Support\Carbon $created_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Book newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Book newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Book query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Book whereAuthor($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Book whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Book whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Book whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Book wherePrice($value)
+ * @mixin \Eloquent
+ */
 class Book extends Model
 {
     //
---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Book;
+
+class BookController extends Controller
+{
+    public function index()
+    {
+        $books = Book::all();
+        return view('book/index', compact('books'));
+    }
+
+    public function edit($id)
+    {
+        $book = Book::findOrFail($id);
+        return view('book/edit', compact('book'));
+    }
+}
